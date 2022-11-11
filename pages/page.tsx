ae8a289ea7f15a9308950f60aad6f4b16d9061f7
Mark Preston <mark@lavabyrd.space>
2022-11-11 22:21:12 +0000

    initialize

---
+++ pages/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [likes, setLikes] = React.useState(0);
+  const [random, setRandom] = React.useState();
+  const callAPI = async () => {
+    try {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_TEST);
+      const data = await res.json();
+      console.log(data);
+      setRandom(data.title);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClick = async () => {
+    setLikes(likes + 1);
+  };
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <button onClick={callAPI}>Make API Call</button>
+        {random}
+        <button onClick={handleClick}>Like({likes})</button>
+        {likes}
+      </main>
+    </div>
+  );
+}
